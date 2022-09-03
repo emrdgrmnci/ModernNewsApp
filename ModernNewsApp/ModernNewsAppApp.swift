102416fb9d7f472e2d142c90753ce2564c667413
EmreDegirmenci <emre.degirmenci@textalkmedia.se>
2022-09-03 17:56:06 +0300

    Initial Commit

---
+++ ModernNewsApp/ModernNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernNewsAppApp.swift
+//  ModernNewsApp
+//
+//  Created by TextalkMedia-Emre on 2022-09-03.
+//
+
+import SwiftUI
+
+@main
+struct ModernNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
